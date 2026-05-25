e191656ac46a1691811cf0b87347a4c10af2f87b
kezialevi <112202407268@mhs.dinus.ac.id>
2026-05-26 02:12:25 +0700

    feat: buat view konfirmasi hapus barang

---
+++ app/views/barang/hapus.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+if (!isset($_SESSION['login'])) {
+    header("Location: ../public/index.php?action=login");
+    exit();
+}
+
+$id = $_GET['id'];
+
+$stmt = $koneksi->prepare("DELETE FROM barang WHERE id=?");
+$stmt->execute([$id]);
+
+header("Location: index.php");
+exit;
+?>
