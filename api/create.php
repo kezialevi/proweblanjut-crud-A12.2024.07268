e05d07575ed6e977d1656f208b76729d5a3102fc
kezialevi <112202407268@mhs.dinus.ac.id>
2026-05-31 13:18:54 +0700

    Mengimplementasikan endpoint create.php untuk operasi tambah data barang

---
+++ api/create.php
@@ -0,0 +1,40 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once "../app/models/BarangModel.php";
+
+$model = new BarangModel();
+
+$data = [
+    'kode_barang'   => $_POST['kode_barang'] ?? '',
+    'nama_barang'   => $_POST['nama_barang'] ?? '',
+    'kategori'      => $_POST['kategori'] ?? '',
+    'jumlah'        => $_POST['jumlah'] ?? '',
+    'harga'         => $_POST['harga'] ?? '',
+    'supplier'      => $_POST['supplier'] ?? '',
+    'tanggal_masuk' => $_POST['tanggal_masuk'] ?? '',
+    'gambar'        => null
+];
+
+if (empty($data['kode_barang'])) {
+    $data['kode_barang'] =
+    $model->generateKodeBarang();
+}
+
+$result = $model->save($data);
+
+if ($result) {
+
+    echo json_encode([
+        "status" => true,
+        "message" => "Barang berhasil ditambahkan"
+    ]);
+
+} else {
+
+    echo json_encode([
+        "status" => false,
+        "message" => "Barang gagal ditambahkan"
+    ]);
+}
