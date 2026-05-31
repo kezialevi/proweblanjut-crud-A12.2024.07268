85c6965942a825d0f8603f6c05e29fbef95f1e9c
kezialevi <112202407268@mhs.dinus.ac.id>
2026-05-31 13:13:56 +0700

    Menambhakn endpoint read untuk menampilkan data barang dalam format JSON

--- api/read.php
+++ api/read.php
@@ -6,34 +6,9 @@ require_once "../app/models/BarangModel.php";
 
 $model = new BarangModel();
 
-$data = [
-    'kode_barang'   => $_POST['kode_barang'] ?? '',
-    'nama_barang'   => $_POST['nama_barang'] ?? '',
-    'kategori'      => $_POST['kategori'] ?? '',
-    'jumlah'        => $_POST['jumlah'] ?? '',
-    'harga'         => $_POST['harga'] ?? '',
-    'supplier'      => $_POST['supplier'] ?? '',
-    'tanggal_masuk' => $_POST['tanggal_masuk'] ?? '',
-    'gambar'        => null
-];
+$data = $model->getAll();
 
-if (empty($data['kode_barang'])) {
-    $data['kode_barang'] =
-    $model->generateKodeBarang();
-}
-
-$result = $model->save($data);
-
-if ($result) {
-    echo json_encode([
-        "status" => true,
-        "message" => "Barang berhasil ditambahkan"
-    ]);
-
-} else {
-
-    echo json_encode([
-        "status" => false,
-        "message" => "Barang gagal ditambahkan"
-    ]);
-}
+echo json_encode(
+    $data,
+    JSON_PRETTY_PRINT
+);
